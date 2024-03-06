b242983abeb724d9f6d1b4f433d1908bac6b3841
petrochongqing <150474653+petrochongqing@users.noreply.github.com>
2024-03-06 11:07:59 +0100

    1

--- v4/layers/_4.js
+++
@@ -1,9 +0,0 @@
-var json__4 = {
-"type": "FeatureCollection",
-"name": "_4",
-"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
-"features": [
-{ "type": "Feature", "properties": { "id": "B0JASC2VB1", "name": "道达尔能源合川高职教城加油站", "type": "汽车服务;加油站;道达尔", "typeCode": 10110.0, "bizType": [ ], "address": "城北大道", "gcj02_lng": 106.24026, "gcj02_lat": 30.031, "wgs84_lng": 106.23647, "wgs84_lat": 30.03366, "tel": [ ], "provinceName": "重庆市", "cityName": "重庆市", "adName": "合川区" }, "geometry": { "type": "Point", "coordinates": [ 106.236466711876616, 30.033655140954981 ] } },
-{ "type": "Feature", "properties": { "id": "B0FFM10BNA", "name": "TOTAL陆航石化赶水服务区加油站(暂停营业)", "type": "汽车服务;加油站;道达尔", "typeCode": 10110.0, "bizType": [ ], "address": "G75兰海高速赶水服务区(遵义方向)", "gcj02_lng": 106.74501, "gcj02_lat": 28.68473, "wgs84_lng": 106.74107, "wgs84_lat": 28.68802, "tel": "023-48799233", "provinceName": "重庆市", "cityName": "重庆市", "adName": "綦江区" }, "geometry": { "type": "Point", "coordinates": [ 106.741066036787927, 28.688024690919576 ] } }
-]
-}
